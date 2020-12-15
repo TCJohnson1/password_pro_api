0aee4ad691b52349e7cbeadf1eb90db90b95eaaf
tcjohnosn2 <terrence.c.johnson2@gmail.com>
2020-12-15 00:37:02 -0600

    Strting express routes

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3001
+require('dotenv').config()
 
-
+app.get('/', (req, res) => {
+      console.log("Start")
+})
 
 app.listen(PORT, () => {
       console.log("listening on port", PORT);
