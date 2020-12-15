74388a75d2efaeb88083f7a3853d628f263eaa5b
tcjohnosn2 <terrence.c.johnson2@gmail.com>
2020-12-15 10:31:04 -0600

    Routes for single passwords and create are complete

--- server.js
+++ server.js
@@ -26,31 +26,40 @@ app.get('/api/v1/passwords', async (req, res) => {
 })
 
 //Get one password
-app.get('/api/v1/passwords/:id', (req, res) => {
-      console.log(req.params)
-      res.status(200).json({
-            status: "success",
-            data: {
-                  website: "google.com",
-                  username: "Tim_Jones",
-                  email: "tim@jones.com",
-                  password: "TJONES1"
-            }
-      })
+app.get('/api/v1/passwords/:website', async (req, res) => {
+      console.log(req.params.website);
+      try {
+            const results = await db.query("select * from passwords where website = $1", [req.params.website]);
+            res.status(200).json({
+                  status: "success",
+                  data: {
+                        passwords: results.rows[0],
+                  }
+            })
+      } catch (err){
+            console.log(err)
+      }
 })
 
 //Create Route
-app.post('/api/v1/passwords', (req, res) =>{
+app.post('/api/v1/passwords', async (req, res) =>{
       console.log(req.body)
-      res.status(201).json({
-            status: "success",
-            data: {
-                  website: "google.com",
-                  username: "Tim_Jones",
-                  email: "tim@jones.com",
-                  password: "TJONES1"
-            }
-      })
+
+      try{
+            const results = await db.query("INSERT INTO passwords (website, username, email, password) values ($1, $2, $3, $4) returning *", [req.body.website, req.body.username, req.body.email, req.body.password])
+            console.log(results)
+            res.status(201).json({
+                  status: "success",
+                  data: {
+                        website: 'Google',
+                        username: "Test",
+                        email: "tim@tim.com",
+                        password: "TJONES1"
+                  }
+            })
+      } catch (err){
+            console.log(err)
+      }
 })
 
 //Update route
@@ -60,7 +69,7 @@ app.put('/api/v1/passwords/:id',(req, res) => {
       res.status(200).json({
             status: "success",
             data: {
-                  website: "google.com",
+                  website: "google",
                   username: "Tim_Jones",
                   email: "tim@jones.com",
                   password: "TJONES1"
